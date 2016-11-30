ef102413ebcea3e67fc2f66f8e17746ccf520ca0
programisti <zaali@live.com>
2016-11-30 15:21:06 +0400

    remvoe instpect

--- lib/meta.ex
+++ lib/meta.ex
@@ -1,6 +1,5 @@
 defmodule Meta do
   def run do
-    IO.inspect "1"
     content = File.read! "ba"
     Code.compile_string("defmodule BAConfig do def get_config do #{content} end end")
     BAConfig.get_config
